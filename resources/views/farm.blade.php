49f01e05b0cac3f06b1bd8ff9b91535781e63a45
benhuang0857 <63923970+benhuang0857@users.noreply.github.com>
2020-11-16 12:16:35 +0800

    fix farm js

--- resources/views/farm.blade.php
+++ resources/views/farm.blade.php
@@ -610,7 +610,7 @@ body::-webkit-scrollbar {
 		<script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="{{ asset('js/f.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('js/farm.js') }}"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
 		<script src="{{ asset('js/jquery.knob.min.js') }}"></script>
 </body>
